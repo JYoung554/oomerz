91cf49dfd469c8d2ad46523422f76a66ce9f55ad
JYoung554 <jontefears10@gmail.com>
2023-09-15 17:30:56 -0400

    created ProfileCard model

--- migrations/20230915212931-create-profile-card.js
+++ migrations/20230915212931-create-profile-card.js
@@ -1,18 +1,27 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('ProfileCards', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userName: {
+      handle: {
         type: Sequelize.STRING
       },
-      password: {
+      avatarUrl: {
         type: Sequelize.STRING
+      },
+      caption: {
+        type: Sequelize.STRING
+      },
+      genStatus: {
+        type: Sequelize.STRING
+      },
+      triviaTotal: {
+        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
@@ -25,6 +34,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('ProfileCards');
   }
 };
---
+++ models/profilecard.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ProfileCard extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  ProfileCard.init({
+    handle: DataTypes.STRING,
+    avatarUrl: DataTypes.STRING,
+    caption: DataTypes.STRING,
+    genStatus: DataTypes.STRING,
+    triviaTotal: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ProfileCard',
+  });
+  return ProfileCard;
+};
