0ff2a5eb4d37a4934efd9afbe878d454e2107a37
JYoung554 <jontefears10@gmail.com>
2023-09-15 18:09:34 -0400

    created Trivia, and FeedCard models

---
+++ migrations/20230915220753-create-trivia-card.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('TriviaCards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      triviaQuestion: {
+        type: Sequelize.STRING
+      },
+      questionNumber: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('TriviaCards');
+  }
+};
---
+++ models/triviacard.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class TriviaCard extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  TriviaCard.init({
+    triviaQuestion: DataTypes.STRING,
+    questionNumber: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'TriviaCard',
+  });
+  return TriviaCard;
+};
