29fbc219e0a2225d30bb42994e1eed37b4a85ee3
JYoung554 <jontefears10@gmail.com>
2023-10-26 11:35:49 -0400

    Set LoginForm component on Login page

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,4 +1,14 @@
 import React from 'react'
 
-const LoginForm = (props) => {}
+const LoginForm = (props) => {
+  return (
+    <div>
+      <form>
+        <input type="text" name="Username" placeholder="Username"></input>
+        <input type="text" name="password" placeholder="Password"></input>
+        <input></input>
+      </form>
+    </div>
+  )
+}
 export default LoginForm
