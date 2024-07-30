6c085299586e1a03145d92906899d8d56db3debe
JYoung554 <jontefears10@gmail.com>
2024-07-30 17:00:07 -0400

    revised .env and package.json files

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcrypt')
 require('dotenv').config()
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const SALT_ROUNDS = 12
 const APP_SECRET = 'evkusbwjodnrnw'
