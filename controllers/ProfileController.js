c083beca20b0b429328e9ca3edfc77a8983bb925
JYoung554 <jontefears10@gmail.com>
2024-08-06 11:54:25 -0400

    included user model in createProfileCard for ProfileCard.js

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -54,7 +54,13 @@ const getOneUser = async (req, res) => {
     let handle = req.params.handle
     const user = await User.findOne({
       attributes: ['id', 'handle', 'avatarUrl'],
-      where: { handle: handle }
+      where: { handle: handle },
+      include: [
+        {
+          model: ProfileCard,
+          attributes: ['id', 'caption', 'genStatus', 'triviaTotal']
+        }
+      ]
     })
     res.send(user)
     console.log(user)
